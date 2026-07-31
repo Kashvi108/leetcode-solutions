b59408ce84e88276da57bc0599fb0e3f101a62ed
Kashvi <aadhishsaini14@gmail.com>
2026-07-31 23:54:58 +0530

    Add solution for Two Sum

---
+++ Arrays/001_Two_Sum.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: Two Sum
+LeetCode: 1
+Difficulty: Easy
+
+Approach:
+Use a hash map to store previously seen numbers.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>hash;
+        for(int i=0;i<nums.size();i++){
+            int required=target-nums[i];
+            if (hash.find (required)!= hash.end()){
+                return {hash [required], i};
+            }
+            hash[nums[i]]=i;
+        }
+        return {};
+    }
+};
